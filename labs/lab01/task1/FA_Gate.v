230dee4824e4ff3bef0808755f3f3b34da273d7d
Arv0535 <f20250535@pilani.bits-pilani.ac.in>
2026-08-29 12:08:58 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -8,16 +8,16 @@
 //           re-simulate with the same tb.v and compare.
 
 module FA_Gate(
-  input  a,
   input  b,
+  input  a,
   input  cin,
-  output sum,
-  output cout
+  output cout,
+  output sum
 );
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
   and (pc1, a,   b);
+  xor (ps,  a,   b);
   xor (sum, cin, ps);
   and (pc2, cin, ps);
   or  (cout, pc1, pc2);
